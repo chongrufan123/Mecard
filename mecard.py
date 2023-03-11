4e0971d059d5cb42b8438013b4357884bfb34ef8
chongrufan123 <chongrufan123@gmail.com>
2023-03-11 23:15:59 +0800

    update add 0.2

--- mecard.py
+++ mecard.py
@@ -3,9 +3,11 @@ import random
 from typing import List
 import datetime  # 添加
 
+import fire
+
 class Card:
     def __init__(self):
-        self.categories = {
+        self.categories_dict = {
             1: '密码学',
             2: '网络安全',
             3: '数据库',
@@ -17,6 +19,47 @@ class Card:
             9: '编程语言',
         }
         self.questions = []
+
+    def load_categories(self):
+        for category, name in self.categories_dict.items():
+                print(f'{category}. {name}')
+            
+    def add(self):
+        self.questions = self.load_questions()
+        self.load_categories()
+        category = input('请选择题目的类别（输入数字）：')
+        if not category.isdigit() or int(category) not in range(1, 10):
+            print('输入错误')
+            return
+        while True:
+            title = input('请输入题目标题：')
+            content = []
+            print('请输入题目内容（以一个单独的句号为结束）：')
+            while True:
+                line = input()
+                if line.strip() == '':
+                    break
+                content.append(line)
+            content = '\n'.join(content).replace('，', ', ').replace('\*', '*').replace('\t', '')
+            question = {'category': int(category), 'title': title, 'content': content, 'times': 0}
+            self.questions.append(question)
+            print('题目添加成功！')
+            print("---------------")
+            juage = input("有以下几种选择：\n保持当前类别：直接按回车；\n更换类别：输入要更换类别的数字\n查看有哪些类别：c；\n结束插入题目：n\n请输入：")
+            if juage == 'c':
+                self.load_categories()
+                category = input('请选择题目的类别（输入数字）：')
+                while not category.isdigit() or int(category) not in range(1, 10):
+                    print('输入错误')
+                    category = input('请选择题目的类别（输入数字）：')
+            if juage == 'n':
+                break
+            if juage.isdigit() and int(juage) in range(1, 10):
+                category = juage
+            print("---------------")
+        with open('card.json', 'w') as f:
+            json.dump(self.questions, f, ensure_ascii=False, indent=4)
+        
 
     def start(self):
         self.questions = self.load_questions()
@@ -31,8 +74,7 @@ class Card:
     def select_category(self) -> int:
         while True:
             print('请选择题库的类别：')
-            for category, name in self.categories.items():
-                print(f'{category}. {name}')
+            self.load_categories()
             choice = input()
             if choice.isdigit() and int(choice) in range(10):
                 return int(choice)
@@ -73,6 +115,7 @@ class Card:
             #     question['times'] += 1
             #     print('回答错误！')
             print(question['content'])
+            print()
             continue_memory = input('是否继续？（y/n）')
             if continue_memory.lower() == 'n':
                 break
@@ -84,3 +127,7 @@ class Card:
             # f.write(f'{accuracy:.2f}%,{total_count},{correct_count},{incorrect_count},{datetime.datetime.now()}\n')
             f.write(f'|{datetime.datetime.now().strftime("%Y-%m-%d")}|{accuracy:.2f}%|{total_count}|{correct_count}|{incorrect_count}|\n')
 
+if __name__ == '__main__':
+    fire.Fire(Card)
+
+# 在这个程序中增加添加题库的功能，命令为"add"，当执行add操作后，询问添加题库的类别，并将所有类别显示在下面，用户可以输入1~9的数字，选择类别，然后是title和content
